82ed3e5166a544bbb53d655cb33792821d16eead
François Pouly <francoispouly@hotmail.fr>
2024-10-02 22:25:40 +0200

    first version with quotes

---
+++ app/models/quote.rb
@@ -0,0 +1,11 @@
+class Quote < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
+  has_one_attached :audio_data
+
+  validates :audio_data, presence: true
+
+  def audio_url
+    polymorphic_url(audio_data, only_path: true)
+  end
+end
