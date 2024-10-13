95d96dd1942509b7a472d81de99cc9e5d4e77678
François Pouly <francoispouly@hotmail.fr>
2024-10-13 21:28:36 +0200

    fix failing tests

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -2,47 +2,51 @@ require "test_helper"
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @person = people(:one)
+    @person = people(:sarkozy)
   end
 
-  test "should get index" do
-    get people_url
-    assert_response :success
-  end
+  class PeopleControllerTestNotLoggedInTest < PeopleControllerTest
 
-  test "should get new" do
-    get new_person_url
-    assert_response :success
-  end
-
-  test "should create person" do
-    assert_difference("Person.count") do
-      post people_url, params: { person: { name: @person.name } }
+    test "should get index" do
+      get people_url
+      assert_response :success
     end
 
-    assert_redirected_to person_url(Person.last)
-  end
-
-  test "should show person" do
-    get person_url(@person)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_person_url(@person)
-    assert_response :success
-  end
-
-  test "should update person" do
-    patch person_url(@person), params: { person: { name: @person.name } }
-    assert_redirected_to person_url(@person)
-  end
-
-  test "should destroy person" do
-    assert_difference("Person.count", -1) do
-      delete person_url(@person)
+    test "should redirect to sign in" do
+      get new_person_url
+      assert_response :redirect
     end
 
-    assert_redirected_to people_url
+    test "should not create person" do
+      assert_difference("Person.count", 0) do
+        post people_url, params: { person: { name: 'Mélenchon' } }
+      end
+
+      assert_response :redirect
+    end
+
+    test "should show person" do
+      get person_url(@person)
+      assert_response :success
+    end
+
+    test "should not get edit when not logged" do
+      get edit_person_url(@person)
+      assert_response :redirect
+    end
+
+    test "should not update person" do
+      patch person_url(@person), params: { person: { name: 'New name' } }
+      assert_equal(@person.name, 'Sarkozy')
+      assert_redirected_to new_user_session_url
+    end
+
+    test "should not destroy person when not logged in" do
+      assert_difference("Person.count", 0) do
+        delete person_url(@person)
+      end
+
+      assert_redirected_to new_user_session_url
+    end
   end
 end
