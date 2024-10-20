5eacf5302dfde69c8e77c6b1fa6b0e96fdbc0265
François Pouly <francoispouly@hotmail.fr>
2024-10-20 15:01:08 +0200

    feat: share quotes with copy/paste

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -14,6 +14,7 @@ class PeopleController < ApplicationController
   # GET /Sarkozy
   def show_by_name
     @person = Person.find_by!(name: params.fetch(:name))
+    @play_id = params[:play_id].to_i
     render :show
   end
 
--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,8 +1,5 @@
-var play_buttons = document.getElementsByClassName("quote-button");
-var people_cards = document.getElementsByClassName("person-card");
-
 function hide_all_share_buttons() {
-    for(let button of play_buttons) {
+    for(let button of document.getElementsByClassName("default-quote-button")) {
         button.classList.remove("hidden");
         document.getElementById("quote_" + button.dataset.id + "_share").classList.add("hidden");
     }
@@ -10,21 +7,50 @@ function hide_all_share_buttons() {
 
 function displayShareButton(quote_id) {
     hide_all_share_buttons();
+    document.getElementById("quote_" + quote_id).classList.add("hidden");
     document.getElementById("quote_" + quote_id + "_share").classList.remove("hidden");
 }
 
-for(let button of play_buttons) {
-    button.onclick = (event) => {
-        if(!event.currentTarget.classList.contains("quote-button-share")) {
-            displayShareButton(event.currentTarget.dataset.id);
-            event.currentTarget.classList.add("hidden");
-        }
-        new Audio(event.currentTarget.dataset.audio).play(); 
+window.onload = function() {
+    var quote_to_play = document.getElementById('quote_to_play');
+    if(quote_to_play != null) {
+        quote_to_play.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
     }
-};
 
-for(let person of people_cards) {
-    person.onclick = (event) => {
-        window.location.href = event.currentTarget.dataset.link; 
+    var play_buttons = document.getElementsByClassName("quote-button");
+    var people_cards = document.getElementsByClassName("person-card");
+    var copy_quote_buttons = document.getElementsByClassName("copy-quote-url");
+
+    for(let button of play_buttons) {
+        button.onclick = (event) => {
+            if(!event.currentTarget.classList.contains("quote-button-share")) {
+                displayShareButton(event.currentTarget.dataset.id);
+                event.currentTarget.classList.add("hidden");
+            }
+            new Audio(event.currentTarget.dataset.audio).play(); 
+        }
+    };
+    
+    for(let person of people_cards) {
+        person.onclick = (event) => {
+            window.location.href = event.currentTarget.dataset.link; 
+        }
+    }
+    
+    for(let button of copy_quote_buttons) {
+        button.onclick = (event) => {
+            var url = window.location.href.split('?')[0];
+            var success_button = document.getElementById("quote_" + button.dataset.id + "_copied");
+            navigator.clipboard.writeText(url + "?play_id=" + button.dataset.id);
+    
+            button.classList.add('hidden');
+            success_button.classList.remove('hidden');
+    
+            // reset to default state
+            setTimeout(() => {
+                button.classList.remove('hidden');
+                success_button.classList.add('hidden');
+            }, 2000);
+        }
     }
 }
